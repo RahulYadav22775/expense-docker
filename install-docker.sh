07196bcfe50e74a83e822106be4fbb3f61ac7c2f
RahulYadav22775 <gopichand556677@gmail.com>
2024-11-26 15:17:16 +0530

    k8

--- install-docker.sh
+++ install-docker.sh
@@ -11,3 +11,36 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
+
+#Disk Resize
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
+#intsalling kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo  mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+
+#intsalling eksctl
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+
